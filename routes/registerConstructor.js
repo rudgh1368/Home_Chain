bb0c4eee324d3bfda5ae31930dd8e4387ba6c508
rudgh1368 <rudgh1368@naver.com>
2019-03-23 21:00:54 +0900

    asd

--- routes/registerConstructor.js
+++ routes/registerConstructor.js
@@ -19,29 +19,45 @@ var registerConstructor = function (req, res) {
         context.output = undefined;
         res.render('registerConstructor.ejs', context);
     }
-}
+};
 
 var register = function (req, res) {
     console.log("registerConstructor/register 접근");
-    var encryptionWallet = req.user.accountEncryption;
-    var walletPassword = req.user.wallet_password;
 
-    var buildingConstructor = req.body.buildingConstructor;
-    var contractAddress = req.body.smartContractAddress;
+    if(!req.user){
+        console.log('사용자 인증 안된 상태임.');
+        res.writeHead('200', {'Content-Type': 'text/html;charset=utf8'});
+        res.write('<script>alert("먼저 로그인해주세요.");' +
+            'location.href="/login"</script>');
+        res.end();
+    } else{
+        var context = {}
+        console.log('사용자 인증된 상태임.');
+        console.log('회원정보 로드.');
+        console.dir(req.user);
+        context.login_success = true;
+        context.user = req.user;
+        context.output = undefined;
 
-    console.log("buildingConstructor : ", buildingConstructor);
+        var encryptionWallet = req.user.accountEncryption;
+        var walletPassword = req.user.wallet_password;
 
-    // 시행사인지 체크
-    // smartcontract 가져오기
+        var buildingConstructor = req.body.buildingConstructor;
+        var contractAddress = req.body.smartContractAddress;
 
-    //accountEncryption, password, contractAddress, buildingConstructor, callback
-    connection.registerBuildingCostructor(encryptionWallet, walletPassword, contractAddress, buildingConstructor, function (result) {
-        if(result) {
-            res.render('registerConstructor.ejs', {output : "success"});
-        }else{
-            // error
-        }
-    });
+        console.log("buildingConstructor : ", buildingConstructor);
+
+        // 시행사인지 체크
+        //accountEncryption, password, contractAddress, buildingConstructor, callback
+        connection.registerBuildingCostructor(encryptionWallet, walletPassword, contractAddress, buildingConstructor, function (result) {
+            if(result) {
+                context.output="success";
+                res.render('registerConstructor.ejs', context);
+            }else{
+                // error
+            }
+        });
+    }
 }
 
 module.exports.registerConstructor = registerConstructor;
