2d2753ec98b92b15583f5b9cdcdb185c444f335c
rudgh1368 <rudgh1368@naver.com>
2019-04-16 16:10:15 +0900

    asd

--- routes/createToken.js
+++ routes/createToken.js
@@ -17,10 +17,15 @@ var createToken = function (req, res) {
             'location.href="/"</script>');
         res.end();
     } else{
-        res.render('createToken.ejs', {output : undefined});
+        var context = {}
+        console.log('사용자 인증된 상태임.');
+        console.log('회원정보 로드.');
+        console.dir(req.user);
+        context.login_success = true;
+        context.user = req.user;
+        context.output = undefined;
+        res.render('createToken.ejs', context);
     }
-
-
 }
 
 var create = function (req, res) {
@@ -30,30 +35,55 @@ var create = function (req, res) {
     console.log("smartContractAddress : ", req.body.smartContractAddress);
     console.log("investmentForm : ", req.body.investmentForm);
 
-    var to = req.body.to;
-    var investmentAmount = req.body.investmentAmount;
-    var smartContractAddress = req.body.smartContractAddress;
-    var investmentForm = req.body.investmentForm;
+    if(!req.user){
+        console.log('사용자 인증 안된 상태임.');
+        res.writeHead('200', {'Content-Type': 'text/html;charset=utf8'});
+        res.write('<script>alert("먼저 로그인해주세요.");' +
+            'location.href="/login"</script>');
+        res.end();
+    } else if (req.user.id != "BANK"){
+        console.log('권한 없는 사용자.');
+        res.writeHead('200', {'Content-Type': 'text/html;charset=utf8'});
+        res.write('<script>alert("권한이 없습니다.");' +
+            'location.href="/"</script>');
+        res.end();
+    } else{
+        var context = {}
+        console.log('사용자 인증된 상태임.');
+        console.log('회원정보 로드.');
+        console.dir(req.user);
+        context.login_success = true;
+        context.user = req.user;
+        context.output = undefined;
+        
+        var to = req.body.to;
+        var investmentAmount = req.body.investmentAmount;
+        var smartContractAddress = req.body.smartContractAddress;
+        var investmentForm = req.body.investmentForm;
 
-    if(to && investmentAmount && smartContractAddress && investmentForm) {
-    console.log("정상입력");
+        if(to && investmentAmount && smartContractAddress && investmentForm) {
+            console.log("정상입력");
 
-    var message = smartContractAddress + "|" + to + "|" + investmentAmount + '|' + investmentForm + "|";
+            var message = smartContractAddress + "|" + to + "|" + investmentAmount + '|' + investmentForm + "|";
 
-    // sign (contractaddress/to/index/investmentAmount) index -> 1 : 투자자, 2: 수분양자
-    connection.signToken(message, function (signedToken) {
-        var json = JSON.stringify(signedToken);
-        var fileName = './' + message + '.json';
+            // sign (contractaddress/to/index/investmentAmount) index -> 1 : 투자자, 2: 수분양자
+            connection.signToken(message, function (signedToken) {
+                var json = JSON.stringify(signedToken);
+                var fileName = './' + message + '.json';
 
-        fs.writeFileSync(fileName ,json, 'utf8');
+                fs.writeFileSync(fileName ,json, 'utf8');
 
-        res.render('createToken.ejs', {output : "success"});
-    });
+                context.output = "success";
+                res.render('createToken.ejs', context);
+            });
 
-    }else{
-        console.log("입력요구");
+        }else{
+            console.log("입력요구");
 
-        res.render('createToken.ejs', {output : "fail"});
+            context.output = "fail";
+            res.render('createToken.ejs', context);
+        }
+
     }
 };
 
